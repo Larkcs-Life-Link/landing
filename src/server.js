d6390c88b5bc58fa2deacf5ed5d7489a7c4153eb
rohithrajasekharan <rohithrajasekharan@outlook.com>
2020-03-23 21:16:56 +0530

    cache busting

--- src/server.js
+++ src/server.js
@@ -71,8 +71,12 @@ const helmet = Helmet.renderStatic();
         <!DOCTYPE html>
         <html lang="en">
           <head>
-          <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
           <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-store" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
             ${helmet.title.toString()}
             ${helmet.meta.toString()}
             ${
