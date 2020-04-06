2ca8964f26a82100681164c7c892d9ae03582ee1
rohithrajasekharan <rohithrajasekharan@outlook.com>
2020-04-06 20:58:50 +0530

    slick

--- src/views/career/CareerStories.js
+++ src/views/career/CareerStories.js
@@ -8,7 +8,7 @@ import ChevronLeft from '@material-ui/icons/ArrowBack';
 import ChevronRight from '@material-ui/icons/ArrowForward';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../../assets/slick.css";
+import "../../assets/headslick.css";
 
 const useStyles = makeStyles(theme => ({
   box : {
