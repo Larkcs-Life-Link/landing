cf3b2110af17d998280ac6813b7fbb6de583e079
rohithrajasekharan <rohithrajasekharan@outlook.com>
2021-05-04 18:52:28 +0530

    git ignore

--- src/constants/Cloudinary_config.js
+++ src/constants/Cloudinary_config.js
@@ -1,9 +1,4 @@
-export default {
-    cloud_name: "dpcberk00",
-    unsigned_upload_preset: "qmz9rbfu"
+  export default {
+    cloud_name: process.env.cloud_name,
+    unsigned_upload_preset: process.env.unsigned_upload_preset
   }
-
-  // export default {
-  //   cloud_name: process.env.cloud_name,
-  //   unsigned_upload_preset: process.env.unsigned_upload_preset
-  // }
